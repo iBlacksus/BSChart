8dcf8132c42b6bc835a49acb3558aa1ac7e092be
weissinet <bwe@weissi.net>
2019-07-16 21:14:50 +0200

    Refactoring BSChartItem, BSChartObject

---
+++ BSChart/DataObjects/BSType.swift
@@ -0,0 +1,16 @@
+//
+//  BSType.swift
+//  BSChart
+//
+//  Created by Bernhard Weiß on 16.07.19.
+//  Copyright © 2019 iBlacksus. All rights reserved.
+//
+
+import Foundation
+
+enum BSType : String {
+    case x
+    case bar
+    case area
+    case line
+}
---
+++ BSChart/Extensions/UIColor+Hex.swift
@@ -0,0 +1,35 @@
+//
+//  UIColor+Hex.swift
+//  BSChart
+//
+//  Created by Bernhard Weiß on 16.07.19.
+//  Copyright © 2019 iBlacksus. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    public convenience init?(hexString: String) {
+        var cString:String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return nil
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
+
